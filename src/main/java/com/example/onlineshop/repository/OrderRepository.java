ff75ef3a149aea40fb6398a134f3630c12133b33
Ivaylo Ivanov <69511821+ivaka4@users.noreply.github.com>
2021-08-26 04:59:18 +0300

    Added order functionality

--- src/main/java/com/example/onlineshop/repository/OrderRepository.java
+++ src/main/java/com/example/onlineshop/repository/OrderRepository.java
@@ -1,9 +1,17 @@
 package com.example.onlineshop.repository;
 
 import com.example.onlineshop.model.entity.OrderEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+    @Query("SELECT o FROM OrderEntity as o WHERE o.productId=:id AND o.isCompleted IN (false)")
+    List<OrderEntity> getOrderEntityByProductId(@Param("id")Long id);
 }
--- src/main/java/com/example/onlineshop/service/impl/StoreServiceImpl.java
+++ src/main/java/com/example/onlineshop/service/impl/StoreServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.onlineshop.service.impl;
 
 import com.example.onlineshop.exception.storeEx.ProductIdNotValid;
+import com.example.onlineshop.model.entity.OrderEntity;
 import com.example.onlineshop.model.entity.StoreEntity;
 import com.example.onlineshop.model.service.StoreServiceModel;
 import com.example.onlineshop.model.view.store.StoreViewModel;
+import com.example.onlineshop.repository.OrderRepository;
 import com.example.onlineshop.repository.StoreRepository;
 import com.example.onlineshop.service.StoreService;
 import org.modelmapper.ModelMapper;
@@ -19,21 +21,24 @@ public class StoreServiceImpl implements StoreService {
 
     private final StoreRepository storeRepository;
     private final ModelMapper modelMapper;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public StoreServiceImpl(StoreRepository storeRepository, ModelMapper modelMapper) {
+    public StoreServiceImpl(StoreRepository storeRepository, ModelMapper modelMapper, OrderRepository orderRepository) {
         this.storeRepository = storeRepository;
         this.modelMapper = modelMapper;
+        this.orderRepository = orderRepository;
     }
 
 
     @Override
     public List<StoreViewModel> getProductsInStock() {
-        List<StoreViewModel> inStockProducts= this.modelMapper
+        List<StoreViewModel> inStockProducts = this.modelMapper
                 .map(this
-                        .storeRepository
-                        .findAllByQuantityGreaterThan(0),
-                        new TypeToken<List<StoreViewModel>>(){}.getType());
+                                .storeRepository
+                                .findAllByQuantityGreaterThan(0),
+                        new TypeToken<List<StoreViewModel>>() {
+                        }.getType());
         return inStockProducts;
     }
 
@@ -45,17 +50,28 @@ public class StoreServiceImpl implements StoreService {
     @Override
     public StoreServiceModel loadProduct(Long productId, int quantity) {
         StoreEntity product = this.storeRepository.findById(productId).orElse(null);
-        if (product == null){
+        if (product == null) {
             throw new ProductIdNotValid("There is no product with this id: " + productId);
         }
+        List<OrderEntity> orderEntity = this.orderRepository.getOrderEntityByProductId(productId);
         product.setQuantity(product.getQuantity() + quantity);
+        for (int i = 0; i < orderEntity.size(); i++) {
+            if (product.getQuantity() > orderEntity.get(i).getNeededQuantity()) {
+                orderEntity.get(i).setQuantity(orderEntity.get(i).getNeededQuantity());
+                product.setQuantity(product.getQuantity() - orderEntity.get(i).getNeededQuantity());
+                orderEntity.get(i).setNeededQuantity(0);
+                orderEntity.get(i).setCompleted(true);
+                this.orderRepository.saveAndFlush(orderEntity.get(i));
+            }
+
+        }
         return this.modelMapper.map(this.storeRepository.saveAndFlush(product), StoreServiceModel.class);
     }
 
     @Override
     public StoreServiceModel addProduct(StoreServiceModel storeServiceModel) {
         StoreEntity newProduct = this.modelMapper.map(storeServiceModel, StoreEntity.class);
-        if (newProduct.getProductName() == null || newProduct.getQuantity() == 0){
+        if (newProduct.getProductName() == null || newProduct.getQuantity() == 0) {
             throw new ProductIdNotValid("Cannot persist model to the database");
         }
         return this.modelMapper.map(this.storeRepository.saveAndFlush(newProduct), StoreServiceModel.class);
@@ -63,7 +79,7 @@ public class StoreServiceImpl implements StoreService {
 
     @Override
     public StoreViewModel getSingleProduct(Long productId) {
-        if (!this.storeRepository.existsById(productId)){
+        if (!this.storeRepository.existsById(productId)) {
             throw new ProductIdNotValid("There is not a product with this id: " + productId);
         }
         return this.modelMapper.map(this.storeRepository.findById(productId).get(), StoreViewModel.class);
